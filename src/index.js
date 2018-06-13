513eeb4af0f18204e0c11a760c0b3e7eff27bd54
vtrushin <monospectr@gmail.com>
2018-06-13 11:02:58 +0300

    Added new examples

--- src/index.js
+++ src/index.js
@@ -3,22 +3,34 @@ const { render, components, createElement } = require('../eml');
 // import { render, components } from '../eml';
 
 const {
-  Eml,
-  Flex,
-  FlexItem
+	AutoGrid,
+	Eml,
+	Flex,
+	FlexItem
 } = components;
 
+// const eml = (
+//   <Eml>
+//     <Flex direction='column' gap={20}>
+//       <FlexItem background={{ color: 'lightblue' }}>
+//         1234
+//       </FlexItem>
+//       <FlexItem background={{ color: 'lightgreen' }}>
+//         4566
+//       </FlexItem>
+//     </Flex>
+//   </Eml>
+// );
+
 const eml = (
-  <Eml>
-    <Flex direction='column' gap={20}>
-      <FlexItem background={{ color: 'lightblue' }}>
-        1234
-      </FlexItem>
-      <FlexItem background={{ color: 'lightgreen' }}>
-        4566
-      </FlexItem>
-    </Flex>
-  </Eml>
+	<div>
+		<AutoGrid>
+			<div>1</div>
+			<div>2</div>
+			<div>3</div>
+			<div>4</div>
+		</AutoGrid>
+	</div>
 );
 
 fs.writeFileSync('./eml.html', render(eml), 'utf8');
