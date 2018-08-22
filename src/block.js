08631d19802a36da2c6ffaf8f3d4b7c15b04465b
vtrushin <monospectr@gmail.com>
2018-08-22 11:09:59 +0300

    Added examples

--- src/block.js
+++ src/block.js
@@ -4,14 +4,57 @@ const {
 	Eml,
 	Block,
 	Link,
+	Text,
 	Flex,
 	Image
 } = components;
 
 export default (
 	<Eml fontFamily="Lato" previewText="Новое письмо!!!" backgroundColor="#ebecee">
-		<Block padding={10} backgroundColor="lightblue">
-			123
-		</Block>
+		<Flex justifyContent="center">
+			<Block width={800} backgroundColor="#fff">
+				<Block padding={[15, 70]}>
+					<Image src="https://www.avito.ru/files/avito/company/logos/Logo-Avito.png" width={100} height={28} />
+				</Block>
+				<Flex
+					direction="column"
+					alignItems="start"
+					justifyContent="stretch"
+					padding={[40, 70]}
+					gap={30}
+					backgroundColor="#344968"
+					color="#fff"
+				>
+					<div>Приближается важный праздник</div>
+					<Link
+						to="https://ya.ru"
+						padding={[20, 50]}
+						backgroundColor="#feb020"
+						color="#ffffff"
+						noUnderline
+					>
+						Урвать скидон
+					</Link>
+				</Flex>
+				<Block padding={[40, 70]}>
+					<Flex direction="column" gap={30} alignItems="start">
+						<div>В дни, когда принято поздравлять ветеранов и радоваться мирному небу, у вас наверняка будет много дел. Но найдите минуточку, чтобы заглянуть в личный кабинет на Авито. Для чего?</div>
+						<Block backgroundColor="#f8f5e2" padding={[20, 30]}>
+							Чтобы использовать акцию по-максимому, добавьте объявление прямо сейчас, а 8 или 9 мая подключите «усилители». Действуйте!
+						</Block>
+						<Link
+							alignSelf="start"
+							to="https://ya.ru"
+							padding={[20, 50]}
+							backgroundColor="#feb020"
+							color="white"
+							noUnderline
+						>
+							Урвать скидон
+						</Link>
+					</Flex>
+				</Block>
+			</Block>
+		</Flex>
 	</Eml>
 );
