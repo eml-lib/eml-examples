6ee0cd5b9eb689cbf1780ef35ef1ac6baa4c73c1
vtrushin <monospectr@gmail.com>
2018-08-26 22:47:31 +0300

    Added examples

--- src/_block.js
+++ src/_block.js
@@ -1,5 +1,7 @@
 const { render, components, createElement } = require('../eml');
 
+const image = fileName => `https://raw.githubusercontent.com/eml-lib/eml-examples/tree/v1.0.0/images/${fileName}`;
+
 const {
 	Eml,
 	Block,
@@ -9,53 +11,51 @@ const {
 } = components;
 
 export default (
-	<Eml
-		// align="center"
-		// maxWidth={780}
-		fontFamily="Lato"
-		previewText="Новое письмо!!!"
-		backgroundColor="#ebecee"
-		// foregroundColor="white"
-	>
-		<Block padding={[15, 70]}>
-			<Image src="https://www.avito.ru/files/avito/company/logos/Logo-Avito.png" width={100} height={28} />
-		</Block>
-		<Flex
-			direction="column"
-			alignItems="start"
-			justifyContent="stretch"
-			padding={[40, 70]}
-			gap={30}
-			background="#344968"
-			color="#ffffff"
-		>
-			Приближается важный праздник
-			<Link
-				to="https://ya.ru"
-				padding={[20, 50]}
-				backgroundColor="#feb020"
-				color="#ffffff"
-				noUnderline
-			>
-				Урвать скидон
-			</Link>
-		</Flex>
-		<Block padding={[40, 70]}>
-			<Flex direction="column" gap={30}>
-				В дни, когда принято поздравлять ветеранов и радоваться мирному небу, у вас наверняка будет много дел. Но найдите минуточку, чтобы заглянуть в личный кабинет на Авито. Для чего?
-				<Block backgroundColor="#f8f5e2" padding={[20, 30]}>
-					Чтобы использовать акцию по-максимому, добавьте объявление прямо сейчас, а 8 или 9 мая подключите «усилители». Действуйте!
+	<Eml fontFamily="Lato" previewText="Новое письмо!!!" backgroundColor="#ebecee">
+		<Flex justifyContent="center">
+			<Block width={800} backgroundColor="#fff">
+				<Block padding={[15, 70]}>
+					<Image src={image('logo.jpg')} width={100} height={28} />
 				</Block>
-				<Link
-					to="https://ya.ru"
-					padding={[20, 50]}
-					backgroundColor="#feb020"
-					color="white"
-					noUnderline
+				<Flex
+					direction="column"
+					alignItems="start"
+					justifyContent="stretch"
+					padding={[40, 70]}
+					gap={30}
+					backgroundColor="#344968"
+					color="#fff"
 				>
-					Урвать скидон
-				</Link>
-			</Flex>
-		</Block>
+					<div>Приближается важный праздник</div>
+					<Link
+						to="https://ya.ru"
+						padding={[20, 50]}
+						backgroundColor="#feb020"
+						color="#ffffff"
+						noUnderline
+					>
+						Урвать скидон
+					</Link>
+				</Flex>
+				<Block padding={[40, 70]}>
+					<Flex direction="column" gap={30} alignItems="start">
+						<div>В дни, когда принято поздравлять ветеранов и радоваться мирному небу, у вас наверняка будет много дел. Но найдите минуточку, чтобы заглянуть в личный кабинет на Авито. Для чего?</div>
+						<Block backgroundColor="#f8f5e2" padding={[20, 30]}>
+							Чтобы использовать акцию по-максимому, добавьте объявление прямо сейчас, а 8 или 9 мая подключите «усилители». Действуйте!
+						</Block>
+						<Link
+							alignSelf="start"
+							to="https://ya.ru"
+							padding={[20, 50]}
+							backgroundColor="#feb020"
+							color="white"
+							noUnderline
+						>
+							Урвать скидон
+						</Link>
+					</Flex>
+				</Block>
+			</Block>
+		</Flex>
 	</Eml>
 );
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 // const { render, components, createElement } = require('../eml');
 const { render, components, createElement } = require('../eml');
 // import { render, components } from '../eml';
-import template from './block';
+import template from './_block';
 // import template from './planning-time';
 // import template from './link';
 // import template from './flex';
